de0c6ed3fb1e4d60aecdffdd92ef983087a0f845
sayemmh <sayemmh@gmail.com>
2017-06-16 09:33:05 -0400

    horizontal jitter working

---
+++ week1/jitter.m
@@ -0,0 +1,27 @@
+% normally distributed horizontal jitter
+
+% I = imread('ngafghan.jpg');
+% I = I(:,:,1);
+function I = jitter(I, lambda)
+
+
+[r, c] = size(I);
+
+% lambda = 10; % max amount of horizontal jitter
+
+a = -lambda; b = lambda;
+
+for row = 1:r
+  displacement = round(rand() * (b - a) + a);
+  if displacement > 0
+    I(row, :) = [zeros(1, displacement), I(row, 1:c-displacement)];
+  elseif displacement < 0
+    displacement = displacement * -1;
+    I(row, :) = [I(row,displacement + 1:end), zeros(1,displacement)];
+  end
+end
+
+imshow(I)
+
+
+end
